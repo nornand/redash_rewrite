864f9bd9125a429170c4f2e4f5da9fd2ac956a9a
nornand <nornand@163.com>
2020-03-20 18:34:31 +0800

    0320

--- client/app/components/EditQueryVisualDialog.jsx
+++ client/app/components/EditQueryVisualDialog.jsx
@@ -5,6 +5,7 @@ import Modal from "antd/lib/modal";
 import Button from "antd/lib/button";
 import Select from "antd/lib/select";
 import Input from "antd/lib/input";
+import Tag from "antd/lib/tag";
 import { wrap as wrapDialog, DialogPropType } from "@/components/DialogWrapper";
 
 
@@ -14,35 +15,54 @@ const SchemaItemType = PropTypes.shape({
   columns: PropTypes.arrayOf(PropTypes.string).isRequired,
 });
 
+
 function FilterColumns(props) {
   const operator = [
     { opr: '=', name: '等于' },
     { opr: '>', name: '大于' }
   ];
-  // const columns=['lucy','lily'];
+  const filterSql = {
+    key: '',
+    opr: '',
+    value: ''
+  };
+  function setFilterSql(type, value) {
+    filterSql[type] = value;
+    props.filterChange(props.index, (filterSql.key + filterSql.opr + filterSql.value));
+  }
+
   return (
     <div className="m-b-10">
-      <Select style={{ width: 100 }}>
+      <Select
+        style={{ width: 100 }}
+        onChange={value => setFilterSql('key', value)}>
         {props.columns.map((item) =>
           <Select.Option key={item} value={item}>
             {item}
           </Select.Option>
         )}
       </Select>
-      <Select className="m-l-5" style={{ width: 70 }}>
+      <Select
+        className="m-l-5"
+        style={{ width: 70 }}
+        onChange={value => setFilterSql('opr', value)}>
         {operator.map((item) =>
           <Select.Option key={item.opr} value={item.opr}>
             {item.name}
           </Select.Option>
         )}
       </Select>
-      <Input className="m-l-5" placeholder='test' style={{ width: '30%' }}></Input>
+      <Input
+        className="m-l-5"
+        placeholder='请输入'
+        style={{ width: '30%' }}
+        onChange={e => setFilterSql('value', e.target.value)}></Input>
       <Button
         className="m-l-5"
         shape="circle"
         type="dashed"
         size="small"
-        onClick={props.filterChange(props.index, '')}>
+        onClick={() => props.filterChange(props.index, '')}>
         <span className="fa fa-trash-o" />
       </Button>
     </div>
@@ -50,20 +70,29 @@ function FilterColumns(props) {
 }
 
 function EditQueryVisualDialog({ dialog, schema }) {
-  const [currentTableIndex, setCurrentTableIndex] = useState(0);
+  const [currentColumns, setCurrentColumns] = useState(schema[0].columns);
   const [filterArray, setFilterArray] = useState([]);
-  function addFilterArray() {
+  const [sortArray, setSortArray] = useState([]);
+  const [addSort, setAddSort] = useState(false);
+  function addFilter() {
     if ((filterArray.length === 0) || (filterArray[filterArray.length - 1] !== ''))
       setFilterArray(filterArray.concat(''));
   }
 
-  function editFilterArray(index, value) {
+  function editFilter(index, value) {
     if (value === '')
       setFilterArray(filterArray.slice(0, index).concat(filterArray.slice(index + 1)))
     else {
       filterArray[index] = value;
-      // setFilterArray(filterArray);
+      setFilterArray(filterArray);
     }
+  }
+
+  function editSort(index, type){
+    if(type==='sort'){
+      sortArray[index].sort = sortArray[index].sort==='A'? 'D':'A';
+    }
+    setSortArray(sortArray);
   }
 
   return (
@@ -74,7 +103,7 @@ function EditQueryVisualDialog({ dialog, schema }) {
           <Select
             className="w-100"
             defaultValue={0}
-            onChange={setCurrentTableIndex}
+            onChange={setCurrentColumns}
           >
             {schema.map((item, index) => (
               <Select.Option key={index} value={index}>
@@ -89,10 +118,10 @@ function EditQueryVisualDialog({ dialog, schema }) {
             mode="multiple"
             allowClear
             style={{ width: '100%' }}
-            defaultValue={schema[currentTableIndex].columns}
-            key={schema[currentTableIndex].columns}
+            defaultValue={currentColumns}
+            key={currentColumns}
           >
-            {schema[currentTableIndex].columns.map(item =>
+            {currentColumns.map(item =>
               <Select.Option key={item}>
                 {item}
               </Select.Option>
@@ -100,17 +129,64 @@ function EditQueryVisualDialog({ dialog, schema }) {
           </Select>
         </div>
         <h5>过滤项</h5>
-        {filterArray.map((item,index) =>
-            <FilterColumns
-              key={index}
-              index={index}
-              columns={schema[currentTableIndex].columns}
-              filterChange={editFilterArray()}>
-            </FilterColumns>
+        {filterArray.map((item, index) =>
+          <FilterColumns
+            key={index}
+            index={index}
+            columns={currentColumns}
+            filterChange={editFilter}>
+          </FilterColumns>
         )}
-        <Button className="m-b-10" type="dashed" size="small" onClick={addFilterArray}>
-          <span className="fa fa-plus" />增加过滤项
+        <Button className="m-b-10" type="dashed" size="small" onClick={addFilter}>
+          <span className="fa fa-plus" /> 增加过滤项
         </Button>
+        {filterArray.map((item, index) =>
+          <Tag
+            key={index}>
+            {item}
+          </Tag>
+        )}
+        <h5>排序</h5>
+        {sortArray.map((item, index) =>
+          <Tag
+            closable
+            // color='green'
+            key={index}
+            checked={item.sort==='D'}
+            onChange={checked=> editSort(index,'sort')}
+            >
+            {item.sort === 'A' ? <span className="fa fa-arrow-up" /> : <span className="fa fa-arrow-down" />}
+            {' '} {item.value}
+          </Tag>
+        )}
+        <Tag 
+          className="m-b-10" 
+          visible={!addSort}
+          onClick={() => setAddSort(true)}
+          style={{ borderStyle: "dashed" }}
+        >
+          <span className="fa fa-plus" />
+          增加排序项
+        </Tag>
+        {addSort === true &&
+          <Select className="m-l-10"
+            visible
+            style={{ width: 100 }}
+            size="small"
+            placeholder="选择列"
+            onChange={value => {
+              setSortArray(sortArray.concat({ value: value, sort: 'A' }));
+              setAddSort(false);
+            }}
+          >
+            {currentColumns.map((item) =>
+              <Select.Option key={item} value={item}>
+                {item}
+              </Select.Option>
+            )}
+          </Select>
+        }
+        <Tag >change</Tag>
       </div>
     </Modal>
   );
